ff613cb1805def501e5122acb7f060f3b357cee9
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-10-21 18:14:28 +0500

    stars in php with for loop

--- index.php
+++ index.php
@@ -17,57 +17,58 @@
 
     <?php
 
-    // for ($i = 1; $i <= 5; $i++) {
-    //     for ($j = 1; $j <= $i; $j++) {
-    //         echo "* &nbsp";
-    //     }
-    //     echo "<br>";
-    // }
+    for ($i = 1; $i <= 5; $i++) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo "* &nbsp";
+        }
+        echo "<br>";
+    }
 
-    // echo "<hr>";
+    echo "<hr>";
 
-    // for ($i = 5; $i >= 1; $i--) {
-    //     for ($j = 1; $j <= $i; $j++) {
-    //         echo "* &nbsp";
-    //     }
-    //     echo "<br>";
-    // }
+    for ($i = 5; $i >= 1; $i--) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo "* &nbsp";
+        }
+        echo "<br>";
+    }
 
-    // echo "<hr>";
+    echo "<hr>";
 
-    // for ($j = 1; $j <= 10; $j = $j + 2) {
-    //     for ($k = 10; $k > $j; $k--) {
-    //         echo "&nbsp";
-    //     }
-    //     for ($l = 1; $l <= $j; $l++) {
-    //         echo "*";
-    //     }
+    for ($j = 1; $j <= 10; $j = $j + 2) {
+        for ($k = 10; $k > $j; $k--) {
+            echo "&nbsp";
+        }
+        for ($l = 1; $l <= $j; $l++) {
+            echo "*";
+        }
 
-    //     echo "<br>";
-    // }
+        echo "<br>";
+    }
 
-    // echo "<hr>";
+    echo "<hr>";
 
 
-    // for ($i = 1; $i <= 5; $i++) {
-    //     for ($j = 1; $j <= $i; $j++) {
-    //         echo "* &nbsp";
-    //     }
-    //     echo "<br>";
-    // }
+    for ($i = 1; $i <= 5; $i++) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo "* &nbsp";
+        }
+        echo "<br>";
+    }
 
-    // echo "<hr>";
+    echo "<hr>";
 
-    // for ($a = 1; $a <= 5; $a++) {
-    //     for ($j = 6; $j > $a; $j--) {
-    //         echo "&nbsp &nbsp";
-    //     }
-    //     for ($b = 1; $b <= $a; $b++) {
-    //         echo " *";
-    //     }
+    for ($a = 1; $a <= 5; $a++) {
+        for ($j = 6; $j > $a; $j--) {
+            echo "&nbsp &nbsp";
+        }
+        for ($b = 1; $b <= $a; $b++) {
+            echo " *";
+        }
 
-    //     echo "<br>";
-    // }
+        echo "<br>";
+    }
+    echo "<hr>";
 
     for ($h = 0; $h <= 5; $h++) {
 
